2723f3ce4dfa11926e73a72daec1c8caa50b2216
Rosie Miller <rosie.miller@arm.com>
2020-03-26 09:25:32 +0000

    ✨ Cmake templates (#3)
    * Initial render/output code

---
+++ mbed_build/_internal/cmake_renderer.py
@@ -0,0 +1,43 @@
+#
+# Copyright (C) 2020 Arm Mbed. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+import pathlib
+
+import jinja2
+
+TEMPLATES_DIRECTORY = pathlib.Path("_internal", "templates")
+TEMPLATE_NAME = "CMakeLists.tmpl"
+
+
+def write_cmakelists_file(output_directory: str) -> None:
+    """Writes out the CMakeLists.txt file to the output directory.
+
+    If the intermediate directories to the output directory don't exist,
+    this function will create them.
+
+    This function will overwrite any existing CMakeLists.txt file in the
+    output directory.
+
+    Args:
+        output_directory: path to directory where we want the CMakeLists.txt to go
+    """
+
+    export_directory = pathlib.Path(output_directory)
+    export_directory.mkdir(parents=True, exist_ok=True)
+
+    output_file = export_directory.joinpath("CMakeLists.txt")
+    output_file.write_text(_render_cmakelists_template())
+
+
+def _render_cmakelists_template() -> str:
+    """Loads the CMakeLists.txt file template and renders it with the correct details.
+
+    Returns:
+        The contents of the rendered CMakeLists.txt template.
+    """
+
+    env = jinja2.Environment(loader=jinja2.PackageLoader("mbed_build", str(TEMPLATES_DIRECTORY)),)
+    template = env.get_template(TEMPLATE_NAME)
+    context = {"hello": "Hello", "world": "World"}
+    return template.render(context)
---
+++ tests/test_cmake_renderer.py
@@ -0,0 +1,32 @@
+#
+# Copyright (C) 2020 Arm Mbed. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+import pathlib
+from unittest import TestCase, mock
+
+from pyfakefs.fake_filesystem_unittest import Patcher
+
+from mbed_build._internal.cmake_renderer import _render_cmakelists_template, write_cmakelists_file
+
+
+class TestWriteCMakeListsFile(TestCase):
+    @mock.patch("mbed_build._internal.cmake_renderer._render_cmakelists_template")
+    def test_writes_content_to_file(self, _render_cmakelists_template):
+        with Patcher():
+            content = "Some rendered content"
+            _render_cmakelists_template.return_value = content
+            export_path = pathlib.Path("tests", "output")
+
+            write_cmakelists_file(str(export_path))
+
+            created_file = pathlib.Path(export_path, "CMakeLists.txt")
+            self.assertTrue(created_file.is_file())
+            self.assertEqual(created_file.read_text(), content)
+
+
+class TestRendersCMakeListsFile(TestCase):
+    def test_returns_rendered_content(self):
+        result = _render_cmakelists_template()
+
+        self.assertEqual(result, "Hello World")
