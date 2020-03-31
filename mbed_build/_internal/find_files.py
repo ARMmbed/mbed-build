4be7a71d365da27a98b68c2c6d9ba5957538cfd8
Bartek Mucha <muchzill4@gmail.com>
2020-03-31 13:19:33 +0100

    Ignore files using .mbedignore

--- mbed_build/_internal/find_files.py
+++ mbed_build/_internal/find_files.py
@@ -3,13 +3,31 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 from pathlib import Path
-from typing import Iterable
+from fnmatch import fnmatch
+from typing import Callable, Iterable
 
 
-def find_files(file_name, root_dir, exclude=[]) -> Iterable[Path]:
+ExcludeCallable = Callable[[Path], bool]
+
+
+def find_files(file_name: str, root_dir: str, exclude: Iterable[ExcludeCallable] = []) -> Iterable[Path]:
     found_files = Path(root_dir).rglob(file_name)
 
     def is_excluded(file):
         return any(fn(file) for fn in exclude)
 
     return (file for file in found_files if not is_excluded(file))
+
+
+def exclude_listed_in_mbedignore(path_to_ignore_file: Path) -> bool:
+    lines = path_to_ignore_file.read_text().splitlines()
+    pattern_lines = (line for line in lines if line.strip() and not line.startswith("#"))
+    ignore_root = path_to_ignore_file.parent
+
+    patterns = tuple(str(ignore_root.joinpath(pattern)) for pattern in pattern_lines)
+
+    def is_excluded(file: Path):
+        is_ignored = any(fnmatch(file, pattern) for pattern in patterns)
+        return is_ignored
+
+    return is_excluded
--- tests/_internal/test_find_files.py
+++ tests/_internal/test_find_files.py
@@ -6,7 +6,7 @@ from unittest import TestCase
 import pathlib
 from pyfakefs.fake_filesystem_unittest import patchfs
 
-from mbed_build._internal.find_files import find_files
+from mbed_build._internal.find_files import find_files, exclude_listed_in_mbedignore
 
 
 class TestFindFiles(TestCase):
@@ -45,3 +45,39 @@ class TestFindFiles(TestCase):
         self.assertIn(file, subject)
         self.assertNotIn(ignored_file_1, subject)
         self.assertNotIn(ignored_file_2, subject)
+
+
+class TestExcludeListedInMbedignore(TestCase):
+    @patchfs
+    def test_returns_true_for_files_matching_mbedignore(self, fs):
+        mbedignore_contents = """
+*.py
+hidden.txt
+
+*/test/*
+*/stubs/*
+stubs/*
+"""
+        fs.create_file("project/.mbedignore", contents=mbedignore_contents)
+
+        subject = exclude_listed_in_mbedignore(pathlib.Path("project/.mbedignore"))
+
+        ignored_paths = (
+            "project/nested/test/file.doc",
+            "project/nested/stubs/file.xls",
+            "project/stubs/file.xls",
+            "project/hidden.txt",
+            "project/file.py",
+        )
+
+        not_ignored_paths = (
+            "outside_of_project/hidden.txt",
+            "foo.py",
+            "project/test/foo.html",
+        )
+
+        for path in ignored_paths:
+            self.assertTrue(subject(pathlib.Path(path)), f"{path} should be ignored")
+
+        for path in not_ignored_paths:
+            self.assertFalse(subject(pathlib.Path(path)), f"{path} should not be ignored")
