8a40ebdba6dbca68789e6bb6dcceac0a655e1d22
Bartek Mucha <muchzill4@gmail.com>
2020-03-31 16:16:22 +0100

    Exclude not labelled and more

--- mbed_build/_internal/find_files.py
+++ mbed_build/_internal/find_files.py
@@ -13,9 +13,32 @@ def find_files(file_name: str, directory: str) -> Iterable[Path]:
 
 
 def exclude_using_mbedignore(mbedignore_file: Path, files: Iterable[Path]) -> Iterable[Path]:
-    """Filters out given Path objects based on `.mbedignore` rules."""
+    """Filters out given Path objects based on `.mbedignore` rules.
+
+    TODO: improve docs - mention that rules are rooted in mbedignore and use unix file matching
+    """
     patterns = _build_mbedignore_patterns(mbedignore_file)
-    return (file for file in files if not _matches_patterns(file, patterns))
+    return (file for file in files if not _matches_mbedignore_patterns(file, patterns))
+
+
+def exclude_not_labelled(label_type: str, label_values: Iterable[str], files: Iterable[Path]) -> Iterable[Path]:
+    """Filters out given Path objects using target labelling rules.
+
+    We distinguish three label types: TARGET, COMPONENT, FEATURE.
+
+    An example of labelled path is "/mbed-os/rtos/source/TARGET_CORTEX/mbed_lib.json",
+    where label type is "TARGET" and label value is "CORTEX".
+
+    This function will filter out labelled paths that don't have matching values for given type.
+
+       >>> exclude_not_labelled(label_type="COMPONENT", labels=["BAR"], files=[
+       ...   pathlib.Path("/COMPONENT_FOO/COMPONENT_BAR/foo.txt"),
+       ...   pathlib.Path("/COMPONENT_BAZ/bar.txt"),
+       ...   pathlib.Path("/TARGET_X/x.txt"),
+       ... ])
+       >>> print(filtered)
+    """
+    return (file for file in files if not _is_labelled_for_different_value(file, label_type, label_values))
 
 
 def _build_mbedignore_patterns(mbedignore_file: Path) -> Iterable[str]:
@@ -26,6 +49,12 @@ def _build_mbedignore_patterns(mbedignore_file: Path) -> Iterable[str]:
     return patterns
 
 
-def _matches_patterns(file: Path, patterns: Iterable[str]) -> bool:
+def _matches_mbedignore_patterns(file: Path, patterns: Iterable[str]) -> bool:
     stringified = str(file)
     return any(fnmatch(stringified, pattern) for pattern in patterns)
+
+
+def _is_labelled_for_different_value(file: Path, label_type: str, label_values: Iterable[str]) -> bool:
+    allowed_labels = set(f"{label_type}_{label_value}" for label_value in label_values)
+    paths_parts_with_label = set(part for part in file.parts if label_type in part)
+    return paths_parts_with_label.issubset(allowed_labels)
--- tests/_internal/test_find_files.py
+++ tests/_internal/test_find_files.py
@@ -6,7 +6,11 @@ from unittest import TestCase
 from pathlib import Path
 from pyfakefs.fake_filesystem_unittest import patchfs
 
-from mbed_build._internal.find_files import find_files, exclude_using_mbedignore
+from mbed_build._internal.find_files import (
+    find_files,
+    exclude_using_mbedignore,
+    exclude_not_labelled,
+)
 
 
 class TestFindFiles(TestCase):
@@ -40,21 +44,42 @@ stubs/*
         mbed_ignore = Path("project", ".mbedignore")
         fs.create_file(mbed_ignore, contents=mbedignore_contents)
 
-        ignored_paths = (
+        paths = [
+            Path("outside_of_project", "hidden.txt"),
+            Path("foo.py"),
+            Path("project", "test", "foo.html"),
+        ]
+
+        excluded_paths = [
             Path("project", "nested", "test", "file.doc"),
             Path("project", "nested", "stubs", "file.xls"),
             Path("project", "stubs", "file.xls"),
             Path("project", "hidden.txt"),
             Path("project", "file.py"),
-        )
+        ]
 
-        not_ignored_paths = (
-            Path("outside_of_project", "hidden.txt"),
-            Path("foo.py"),
-            Path("project", "test", "foo.html"),
-        )
-
-        subject = exclude_using_mbedignore(mbed_ignore, ignored_paths + not_ignored_paths)
+        subject = exclude_using_mbedignore(mbed_ignore, paths + excluded_paths)
 
         for path in subject:
-            self.assertIn(path, not_ignored_paths, f"{path} should be ignored")
+            self.assertIn(path, paths, f"{path} should be excluded")
+
+
+class TestExcludeNotLabelled(TestCase):
+    def test_excludes_files_not_matching_label(self):
+
+        paths = [
+            Path("mbed-os", "TARGET_BAR", "some_file.c"),
+            Path("mbed-os", "COMPONENT_X", "header.h"),
+            Path("mbed-os", "COMPONENT_X", "header.h"),
+            Path("README.md"),
+        ]
+
+        excluded_paths = [
+            Path("mbed-os", "TARGET_FOO", "some_file.c"),
+            Path("mbed-os", "TARGET_FOO", "nested", "other_file.c"),
+        ]
+
+        subject = exclude_not_labelled(label_type="TARGET", label_values=["FOO"], files=(paths + excluded_paths))
+
+        for path in subject:
+            self.assertIn(path, paths, f"{path} should be excluded")
