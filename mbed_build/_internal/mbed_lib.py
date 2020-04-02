10066bb1fd40362ce0cc95442926644cc8a3625b
Bartek Mucha <muchzill4@gmail.com>
2020-04-02 11:47:40 +0100

    Integrate filtering by target labels

--- mbed_build/_internal/mbed_lib.py
+++ mbed_build/_internal/mbed_lib.py
@@ -4,17 +4,40 @@
 #
 """Mbed lib file scanner."""
 from pathlib import Path
-from typing import Iterable
+from typing import Dict, Iterable
 
-from mbed_build._internal.find_files import find_files, exclude_using_mbedignore
+from mbed_build._internal.find_files import (
+    find_files,
+    exclude_using_mbedignore,
+    exclude_not_labelled,
+)
 
 
-def find_mbed_lib_files(directory: Path) -> Iterable[Path]:
-    """Finds all mbed lib files."""
-    mbed_lib_paths = find_files("mbed_lib.json", directory)
+def find_mbed_lib_files(mbed_program_directory: Path, board_type: str) -> Iterable[Path]:
+    """Return paths to all mbed_lib.json for a given target.
 
-    mbedignore_paths = find_files(".mbedignore", directory)
+    When discovering mbed_lib.json files, the following filtering is applied:
+    - paths matching patterns from .mbedignore files will be excluded
+    - paths not matching target labels will be excluded
+
+    Args:
+        mbed_program_directory: Location of mbed program
+        board_type: Name of the target to filter files for
+    """
+    mbed_lib_paths = find_files("mbed_lib.json", mbed_program_directory)
+
+    mbedignore_paths = find_files(".mbedignore", mbed_program_directory)
     for mbedignore_path in mbedignore_paths:
         mbed_lib_paths = exclude_using_mbedignore(mbedignore_path, mbed_lib_paths)
 
+    target_labels = _get_all_target_labels(mbed_program_directory, board_type)
+    if target_labels:
+        for label_type, allowed_label_values in target_labels.items():
+            mbed_lib_paths = exclude_not_labelled(label_type, allowed_label_values, mbed_lib_paths)
+
     return mbed_lib_paths
+
+
+def _get_all_target_labels(mbed_program_directory: Path, board_type: str) -> Dict:
+    """TODO."""
+    pass
--- tests/_internal/test_mbed_lib.py
+++ tests/_internal/test_mbed_lib.py
@@ -4,7 +4,7 @@
 #
 from pathlib import Path
 from pyfakefs.fake_filesystem_unittest import patchfs
-from unittest import TestCase
+from unittest import TestCase, mock
 
 from mbed_build._internal.mbed_lib import find_mbed_lib_files
 
@@ -20,7 +20,7 @@ class TestFindMbedLibFiles(TestCase):
         for path in mbed_lib_paths:
             fs.create_file(path)
 
-        subject = find_mbed_lib_files(Path("root"))
+        subject = find_mbed_lib_files(Path("root"), board_type="K64F")
 
         for path in mbed_lib_paths:
             self.assertIn(path, subject)
@@ -45,7 +45,30 @@ ignored_2/mbed_lib.json
 """
         fs.create_file(mbedignore_path, contents=mbedignore_contents)
 
-        subject = find_mbed_lib_files(Path("root"))
+        subject = find_mbed_lib_files(Path("root"), board_type="K66F")
 
         for path in subject:
             self.assertNotIn(path, ignored_mbed_lib_paths, f"{path} should be ignored")
+
+    @patchfs
+    @mock.patch("mbed_build._internal.mbed_lib._get_all_target_labels", autospec=True)
+    def test_respects_labelling_rules(self, _get_all_target_labels, fs):
+        mbed_program_directory = Path("not_important")
+        board_type = "K64F"
+        _get_all_target_labels.return_value = {"TARGET": ["FOO", "BAR"]}
+        mbed_lib_paths = (
+            Path("root", "TARGET_FOO", "mbed_lib.json"),
+            Path("root", "TARGET_FOO", "TARGET_BAR", "mbed_lib.json"),
+        )
+        ignored_mbed_lib_paths = (
+            Path("root", "TARGET_IGNORED", "mbed_lib.json"),
+            Path("root", "TARGET_FOO", "TARGET_IGNORED", "mbed_lib.json"),
+        )
+        for path in mbed_lib_paths + ignored_mbed_lib_paths:
+            fs.create_file(path)
+
+        subject = find_mbed_lib_files(mbed_program_directory, board_type)
+
+        for path in subject:
+            self.assertNotIn(path, ignored_mbed_lib_paths, f"{path} should be ignored")
+        _get_all_target_labels.assert_called_once_with(mbed_program_directory, board_type)
