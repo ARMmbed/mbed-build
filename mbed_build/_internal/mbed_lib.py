cc7584796b5ce65f5619bf44a01caf1648ee285f
Bartek Mucha <muchzill4@gmail.com>
2020-04-02 15:37:44 +0100

    Refactor #1

--- mbed_build/_internal/mbed_lib.py
+++ mbed_build/_internal/mbed_lib.py
@@ -4,46 +4,34 @@
 #
 """Mbed lib file scanner."""
 from pathlib import Path
-from typing import Dict, Iterable
+from typing import Iterable
 
 from mbed_targets import get_build_attributes_by_board_type
 
 from mbed_build._internal.find_files import (
     find_files,
     exclude_using_mbedignore,
-    exclude_not_labelled,
+    exclude_using_labels,
 )
 
 
 def find_mbed_lib_files(mbed_program_directory: Path, board_type: str) -> Iterable[Path]:
     """Return paths to all mbed_lib.json for a given target.
 
-    When discovering mbed_lib.json files, the following filtering is applied:
-    - paths matching patterns from .mbedignore files will be excluded
-    - paths not matching target labels will be excluded
-
     Args:
         mbed_program_directory: Location of mbed program
         board_type: Name of the target to filter files for
     """
     mbed_lib_paths = find_files("mbed_lib.json", mbed_program_directory)
-
-    mbedignore_paths = find_files(".mbedignore", mbed_program_directory)
-    for mbedignore_path in mbedignore_paths:
-        mbed_lib_paths = exclude_using_mbedignore(mbedignore_path, mbed_lib_paths)
-
-    target_labels = _get_all_target_labels(mbed_program_directory, board_type)
-    for label_type, allowed_label_values in target_labels.items():
-        mbed_lib_paths = exclude_not_labelled(label_type, allowed_label_values, mbed_lib_paths)
-
+    mbed_lib_paths = exclude_using_mbedignore(mbed_program_directory, mbed_lib_paths)
+    mbed_lib_paths = _exclude_using_target_labels(mbed_program_directory, board_type, mbed_lib_paths)
     return mbed_lib_paths
 
 
-def _get_all_target_labels(mbed_program_directory: Path, board_type: str) -> Dict:
-    """Return all labels for a given target, grouped by type."""
+def _exclude_using_target_labels(mbed_program_directory, board_type, paths):
+    """Filter out given paths using target labels."""
     build_attributes = get_build_attributes_by_board_type(board_type, mbed_program_directory)
-    return {
-        "TARGET": build_attributes.labels,
-        "FEATURE": build_attributes.features,
-        "COMPONENT": build_attributes.components,
-    }
+    paths = exclude_using_labels("TARGET", build_attributes.labels, paths)
+    paths = exclude_using_labels("FEATURE", build_attributes.features, paths)
+    paths = exclude_using_labels("COMPONENT", build_attributes.components, paths)
+    return paths
--- tests/_internal/test_mbed_lib.py
+++ tests/_internal/test_mbed_lib.py
@@ -3,98 +3,57 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 from pathlib import Path
-from pyfakefs.fake_filesystem_unittest import patchfs
 from unittest import TestCase, mock
 
-from mbed_build._internal.mbed_lib import find_mbed_lib_files, _get_all_target_labels
+from mbed_build._internal.mbed_lib import find_mbed_lib_files, _exclude_using_target_labels
 from mbed_targets import MbedTargetBuildAttributes
 
 
 class TestFindMbedLibFiles(TestCase):
-    @patchfs
-    @mock.patch("mbed_build._internal.mbed_lib._get_all_target_labels", return_value={}, autospec=True)
-    def test_finds_all_mbed_lib_files(self, _get_all_target_labels, fs):
-        mbed_lib_paths = (
-            Path("root", "mbed_lib.json"),
-            Path("root", "foo", "mbed_lib.json"),
-            Path("root", "foo", "bar", "mbed_lib.json"),
-        )
-        for path in mbed_lib_paths:
-            fs.create_file(path)
-
-        subject = find_mbed_lib_files(Path("root"), board_type="K64F")
-
-        for path in mbed_lib_paths:
-            self.assertIn(path, subject)
-
-    @patchfs
-    @mock.patch("mbed_build._internal.mbed_lib._get_all_target_labels", return_value={}, autospec=True)
-    def test_respects_mbedignore(self, _get_all_target_labels, fs):
-        mbed_lib_paths = (
-            Path("root", "mbed_lib.json"),
-            Path("root", "foo", "mbed_lib.json"),
-            Path("root", "foo", "bar", "mbed_lib.json"),
-        )
-        ignored_mbed_lib_paths = (
-            Path("root", "foo", "ignored_1", "mbed_lib.json"),
-            Path("root", "foo", "ignored_2", "mbed_lib.json"),
-        )
-        for path in mbed_lib_paths + ignored_mbed_lib_paths:
-            fs.create_file(path)
-        mbedignore_path = Path("root", "foo", ".mbedignore")
-        mbedignore_contents = """
-ignored_1/*
-ignored_2/mbed_lib.json
-"""
-        fs.create_file(mbedignore_path, contents=mbedignore_contents)
-
-        subject = find_mbed_lib_files(Path("root"), board_type="K66F")
-
-        for path in subject:
-            self.assertNotIn(path, ignored_mbed_lib_paths, f"{path} should be ignored")
-
-    @patchfs
-    @mock.patch("mbed_build._internal.mbed_lib._get_all_target_labels", return_value={}, autospec=True)
-    def test_respects_labelling_rules(self, _get_all_target_labels, fs):
-        mbed_program_directory = Path("not_important")
+    @mock.patch("mbed_build._internal.mbed_lib.find_files", autospec=True)
+    @mock.patch("mbed_build._internal.mbed_lib.exclude_using_mbedignore", autospec=True)
+    @mock.patch("mbed_build._internal.mbed_lib._exclude_using_target_labels", autospec=True)
+    def test_filters_mbed_lib_json_paths_using_exclusion_rules(
+        self, _exclude_using_target_labels, exclude_using_mbedignore, find_files
+    ):
+        mbed_program_directory = Path("some-program")
         board_type = "K64F"
-        _get_all_target_labels.return_value = {"TARGET": ["FOO", "BAR"]}
-        mbed_lib_paths = (
-            Path("root", "TARGET_FOO", "mbed_lib.json"),
-            Path("root", "TARGET_FOO", "TARGET_BAR", "mbed_lib.json"),
-        )
-        ignored_mbed_lib_paths = (
-            Path("root", "TARGET_IGNORED", "mbed_lib.json"),
-            Path("root", "TARGET_FOO", "TARGET_IGNORED", "mbed_lib.json"),
-        )
-        for path in mbed_lib_paths + ignored_mbed_lib_paths:
-            fs.create_file(path)
 
         subject = find_mbed_lib_files(mbed_program_directory, board_type)
 
-        for path in subject:
-            self.assertNotIn(path, ignored_mbed_lib_paths, f"{path} should be ignored")
-        _get_all_target_labels.assert_called_once_with(mbed_program_directory, board_type)
+        self.assertEqual(subject, _exclude_using_target_labels.return_value)
+        find_files.assert_called_once_with("mbed_lib.json", mbed_program_directory)
+        exclude_using_mbedignore.assert_called_once_with(mbed_program_directory, find_files.return_value)
+        _exclude_using_target_labels.assert_called_once_with(
+            mbed_program_directory, board_type, exclude_using_mbedignore.return_value
+        )
 
 
-class TestGetAllTargetLabels(TestCase):
+class TestExcludeUsingTargetLabels(TestCase):
     @mock.patch("mbed_build._internal.mbed_lib.get_build_attributes_by_board_type", autospec=True)
-    def test_returns_labels_extracted_from_build_attributes(self, get_build_attributes_by_board_type):
+    @mock.patch("mbed_build._internal.mbed_lib.exclude_using_labels", autospec=True)
+    def test_excludes_paths_using_target_labels(self, exclude_using_labels, get_build_attributes_by_board_type):
         build_attributes = mock.Mock(
             MbedTargetBuildAttributes, labels={"TARGET"}, features={"FEATURE"}, components={"COMPONENT"}
         )  # Zero interface safety here, dataclasses don't support spec_set
         get_build_attributes_by_board_type.return_value = build_attributes
+        after_target_filtering = [Path("filtered_using_targets")]
+        after_feature_filtering = [Path("filtered_using_features")]
+        after_component_filtering = [Path("filtered_using_components")]
+        exclude_using_labels.side_effect = [after_target_filtering, after_feature_filtering, after_component_filtering]
         mbed_program_directory = Path("some-path")
         board_type = "A_TYPE"
+        paths = [Path("mbed_lib.json")]
 
-        subject = _get_all_target_labels(mbed_program_directory, board_type)
+        subject = _exclude_using_target_labels(mbed_program_directory, board_type, paths)
 
-        self.assertEqual(
-            subject,
-            {
-                "TARGET": build_attributes.labels,
-                "FEATURE": build_attributes.features,
-                "COMPONENT": build_attributes.components,
-            },
-        )
+        self.assertEqual(subject, after_component_filtering)
         get_build_attributes_by_board_type.assert_called_once_with(board_type, mbed_program_directory)
+        self.assertEqual(
+            exclude_using_labels.mock_calls,
+            [
+                mock.call("TARGET", build_attributes.labels, paths),
+                mock.call("FEATURE", build_attributes.features, after_target_filtering),
+                mock.call("COMPONENT", build_attributes.components, after_feature_filtering),
+            ],
+        )
