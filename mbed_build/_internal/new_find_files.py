a824af2769b0a5627ce501553d30e09d53c49524
Bartek Mucha <muchzill4@gmail.com>
2020-04-04 14:14:23 +0100

    Mbedignore

--- mbed_build/_internal/new_find_files.py
+++ mbed_build/_internal/new_find_files.py
@@ -6,6 +6,29 @@ import fnmatch
 from mbed_targets import get_build_attributes_by_board_type
 
 
+def list_files(desired_filename, program_directory):
+    filters = []
+    result = []
+
+    for dirpath, dirnames, filenames in os.walk(program_directory):
+        if ".mbedignore" in filenames:
+            # Prepend to filters, as .mbedignore should match more paths to filter out
+            filters.append(ExcludeMatchingMbedignore.from_file(Path(dirpath, ".mbedignore")))
+
+        if not all(f(Path(dirpath)) for f in filters):
+            continue
+
+        for filename in filenames:
+            file_path = Path(dirpath, filename)
+            if not all(f(file_path) for f in filters):
+                continue
+
+            if desired_filename == filename:
+                result.append(Path(dirpath, filename))
+
+    return result
+
+
 class MatchFilename:
     def __init__(self, filename):
         self._filename = filename
@@ -14,53 +37,41 @@ class MatchFilename:
         return path.name == self._filename
 
 
-class ExcludeUsingTargetLabels:
-    def __init__(self, mbed_program_directory, board_type):
-        build_attributes = get_build_attributes_by_board_type(board_type, mbed_program_directory)
-        self._filters = [
-            ExcludeUsingLabels("TARGET", build_attributes.labels),
-            ExcludeUsingLabels("FEATURE", build_attributes.features),
-            ExcludeUsingLabels("COMPONENT", build_attributes.components),
-        ]
+# class ExcludeUsingTargetLabels:
+#     def __init__(self, mbed_program_directory, board_type):
+#         build_attributes = get_build_attributes_by_board_type(board_type, mbed_program_directory)
+#         self._filters = [
+#             ExcludeUsingLabels("TARGET", build_attributes.labels),
+#             ExcludeUsingLabels("FEATURE", build_attributes.features),
+#             ExcludeUsingLabels("COMPONENT", build_attributes.components),
+#         ]
 
-    def __call__(self, path):
-        return all(f(path) for f in self._filters)
+#     def __call__(self, path):
+#         return all(f(path) for f in self._filters)
 
 
-class ExcludeUsingLabels:
-    def __init__(self, label_type, allowed_label_values):
-        self._label_type = label_type
-        self._allowed_labels = set(f"{label_type}_{label_value}" for label_value in allowed_label_values)
+# class ExcludeUsingLabels:
+#     def __init__(self, label_type, allowed_label_values):
+#         self._label_type = label_type
+#         self._allowed_labels = set(f"{label_type}_{label_value}" for label_value in allowed_label_values)
 
-    def __call__(self, path):
-        labels = set(part for part in path.parts if self._label_type in part)
-        return labels.issubset(self._allowed_labels)
+#     def __call__(self, path):
+#         labels = set(part for part in path.parts if self._label_type in part)
+#         return labels.issubset(self._allowed_labels)
 
 
 class ExcludeMatchingMbedignore:
-    def __init__(self, mbedignore_path):
-        self._patterns = self._build_mbedignore_patterns(mbedignore_path)
+    def __init__(self, patterns):
+        self._patterns = patterns
 
     def __call__(self, path):
         stringified = str(path)
-        # all?
         return not any(fnmatch.fnmatch(stringified, pattern) for pattern in self._patterns)
 
-
-def find_files(desired_filename, program_directory):
-    # filters = collections.deque([ExcludeUsingTargetLabels(program_directory, board_type)])
-
-    result = []
-
-    for dirpath, dirnames, filenames in os.walk(program_directory):
-        # if ".mbedignore" in filenames:
-        #     # Prepend to filters, as .mbedignore should match more paths to filter out
-        #     filters.appendleft(ExcludeMatchingMbedignore(Path(dirpath, ".mbedignore")))
-
-        # if not all(f(dirpath) for f in filters):
-        #     continue
-        for filename in filenames:
-            if desired_filename == filename:
-                result.append(Path(dirpath, filename))
-
-    return result
+    @classmethod
+    def from_file(cls, mbedignore_path):
+        lines = mbedignore_path.read_text().splitlines()
+        pattern_lines = (line for line in lines if line.strip() and not line.startswith("#"))
+        ignore_root = mbedignore_path.parent
+        patterns = tuple(str(ignore_root.joinpath(pattern)) for pattern in pattern_lines)
+        return cls(patterns)
--- tests/_internal/test_new_find_files.py
+++ tests/_internal/test_new_find_files.py
@@ -4,7 +4,7 @@ from tempfile import TemporaryDirectory
 from unittest import TestCase
 from typing import Iterable
 
-from mbed_build._internal.new_find_files import find_files
+from mbed_build._internal.new_find_files import list_files, ExcludeMatchingMbedignore
 
 
 @contextlib.contextmanager
@@ -20,17 +20,58 @@ def create_files(files: Iterable[Path]):
         yield Path(temp_directory)
 
 
-class TestFindFiles(TestCase):
+class TestListFiles(TestCase):
     def test_finds_files_by_name(self):
-        test_files = [
+        matching_paths = [
             Path("file.txt"),
-            Path("not_interested.txt"),
             Path("sub_directory", "file.txt"),
+        ]
+        excluded_paths = [
+            Path("not_interested.txt"),
             Path("sub_directory", "not_interested.txt"),
         ]
-        with create_files(test_files) as program_directory:
-            subject = find_files("file.txt", program_directory)
 
-        self.assertEqual(
-            subject, [Path(program_directory, "file.txt"), Path(program_directory, "sub_directory", "file.txt")]
-        )
+        with create_files(matching_paths + excluded_paths) as program_directory:
+            subject = list_files("file.txt", program_directory)
+
+        self.assertEqual(len(subject), len(matching_paths))
+        for path in matching_paths:
+            self.assertIn(Path(program_directory, path), subject)
+
+    def test_respects_mbedignore(self):
+        matching_paths = [
+            Path("file.txt"),
+        ]
+        excluded_paths = [
+            Path("foo", "file.txt"),
+            Path("bar", "file.txt"),
+        ]
+        with create_files(matching_paths + excluded_paths) as program_directory:
+            Path(program_directory, ".mbedignore").write_text("foo/*")
+            Path(program_directory, "bar", ".mbedignore").write_text("*")
+
+            subject = list_files("file.txt", program_directory)
+
+        self.assertEqual(len(subject), len(matching_paths))
+        for path in matching_paths:
+            self.assertIn(Path(program_directory, path), subject)
+
+
+class TestExcludeUsingMbedignore(TestCase):
+    def test_matches_files_by_name(self):
+        subject = ExcludeMatchingMbedignore(("*.py",))
+
+        self.assertFalse(subject("file.py"))
+        self.assertFalse(subject("nested/file.py"))
+        self.assertTrue(subject("file.txt"))
+
+    def test_matches_wildcards(self):
+        subject = ExcludeMatchingMbedignore(("*/test/*",))
+
+        self.assertFalse(subject("foo/test/bar.txt"))
+        self.assertFalse(subject("bar/test/other/file.py"))
+        self.assertTrue(subject("file.txt"))
+
+    def test_from_file(self):
+        # TODO:
+        pass
