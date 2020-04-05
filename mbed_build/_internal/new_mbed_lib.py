492515bfb1bd0382db850f76cd9e2dbc2a31bca4
Bartek Mucha <muchzill4@gmail.com>
2020-04-05 10:27:22 +0100

    Use BoardLabelFilter

--- mbed_build/_internal/new_mbed_lib.py
+++ mbed_build/_internal/new_mbed_lib.py
@@ -5,9 +5,8 @@
 """Mbed lib file scanner."""
 from pathlib import Path
 from typing import Iterable
-from mbed_targets import get_build_attributes_by_board_type
 
-from mbed_build._internal.new_find_files import find_files, LabelFilter
+from mbed_build._internal.new_find_files import find_files, BoardLabelFilter
 
 
 def find_mbed_lib_files(mbed_program_directory: Path, board_type: str) -> Iterable[Path]:
@@ -17,10 +16,4 @@ def find_mbed_lib_files(mbed_program_directory: Path, board_type: str) -> Iterab
         mbed_program_directory: Location of mbed program
         board_type: Name of the target to filter files for
     """
-    build_attributes = get_build_attributes_by_board_type(board_type, mbed_program_directory)
-    label_filters = [
-        LabelFilter("TARGET", build_attributes.labels),
-        LabelFilter("FEATURE", build_attributes.features),
-        LabelFilter("COMPONENT", build_attributes.components),
-    ]
-    return find_files("mbed_lib.json", mbed_program_directory, label_filters)
+    return find_files("mbed_lib.json", mbed_program_directory, [BoardLabelFilter(board_type, mbed_program_directory)])
