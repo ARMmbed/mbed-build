d0f02431ccb85a131272e831ada035e173cd2765
Rosie Miller <rosie.miller@arm.com>
2020-04-08 21:37:18 +0100

    Give the user more feedback from the export command.

--- mbed_build/_internal/mbed_tools/export.py
+++ mbed_build/_internal/mbed_tools/export.py
@@ -26,7 +26,9 @@ from mbed_build.mbed_build import generate_cmakelists_file, write_cmakelists_fil
     help="The toolchain you are using to build your app.",
 )
 @click.option("-m", "--mbed_target", required=True, help="A build target for an Mbed-enabled device, eg. K64F")
-@click.option("-p", "--project-path", required=True, help="Path to local Mbed project")
+@click.option(
+    "-p", "--project-path", default=".", help="Path to local Mbed project. By default is the current working directory."
+)
 def export(output_directory: str, toolchain: str, mbed_target: str, project_path: str) -> None:
     """Exports a top-level CMakeLists.txt file to the specified directory.
 
@@ -45,3 +47,4 @@ def export(output_directory: str, toolchain: str, mbed_target: str, project_path
     """
     cmake_file_contents = generate_cmakelists_file(mbed_target, project_path, toolchain)
     write_cmakelists_file(pathlib.Path(output_directory), cmake_file_contents)
+    click.echo(f"The program-level CMake file has been exported to f{str(output_directory)}")
