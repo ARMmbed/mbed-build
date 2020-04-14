b8854b5729a08de0e5d433adf744e061811f3652
Rosie Miller <rosie.miller@arm.com>
2020-04-14 14:40:29 +0100

    Simpler export tests

--- tests/_internal/mbed_tools/test_export.py
+++ tests/_internal/mbed_tools/test_export.py
@@ -11,10 +11,9 @@ from mbed_build._internal.mbed_tools.export import export
 
 
 class TestExport(TestCase):
-    @mock.patch("mbed_build._internal.mbed_tools.export.click.echo")
     @mock.patch("mbed_build._internal.mbed_tools.export.generate_cmakelists_file")
     @mock.patch("mbed_build._internal.mbed_tools.export.write_cmakelists_file")
-    def test_export(self, mock_write_cmakelists_file, mock_generate_cmakelists_file, mock_echo):
+    def test_export(self, mock_write_cmakelists_file, mock_generate_cmakelists_file):
         mock_file_contents = "Hello world"
         mock_generate_cmakelists_file.return_value = mock_file_contents
         output_dir = "some-directory"
@@ -26,18 +25,15 @@ class TestExport(TestCase):
         result = runner.invoke(export, ["-o", output_dir, "-t", toolchain, "-m", mbed_target, "-p", project_path])
 
         self.assertEqual(result.exit_code, 0)
+        self.assertIn(output_dir, result.output)
         mock_generate_cmakelists_file.assert_called_once_with(mbed_target, project_path, toolchain)
         mock_write_cmakelists_file.assert_called_once_with(
             pathlib.Path(output_dir), mock_generate_cmakelists_file.return_value
         )
-        mock_echo.assert_called_once_with(
-            f"The program-level CMake file has been successfully exported to directory '{str(output_dir)}'"
-        )
 
-    @mock.patch("mbed_build._internal.mbed_tools.export.click.echo")
     @mock.patch("mbed_build._internal.mbed_tools.export.generate_cmakelists_file")
     @mock.patch("mbed_build._internal.mbed_tools.export.write_cmakelists_file")
-    def test_export_default_project_path(self, mock_write_cmakelists_file, mock_generate_cmakelists_file, mock_echo):
+    def test_export_default_project_path(self, mock_write_cmakelists_file, mock_generate_cmakelists_file):
         mock_file_contents = "Hello world"
         mock_generate_cmakelists_file.return_value = mock_file_contents
         output_dir = "some-directory"
@@ -48,10 +44,8 @@ class TestExport(TestCase):
         result = runner.invoke(export, ["-o", output_dir, "-t", toolchain, "-m", mbed_target])
 
         self.assertEqual(result.exit_code, 0)
+        self.assertIn(output_dir, result.output)
         mock_generate_cmakelists_file.assert_called_once_with(mbed_target, ".", toolchain)
         mock_write_cmakelists_file.assert_called_once_with(
             pathlib.Path(output_dir), mock_generate_cmakelists_file.return_value
-        )
-        mock_echo.assert_called_once_with(
-            f"The program-level CMake file has been successfully exported to directory '{str(output_dir)}'"
         )
