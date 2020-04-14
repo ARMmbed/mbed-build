10153bdd26352220b8ba5f75738123e048c5cf3f
Bartek Mucha <muchzill4@gmail.com>
2020-04-14 16:31:39 +0100

    Simplify config test

--- tests/_internal/config/factories.py
+++ tests/_internal/config/factories.py
@@ -2,27 +2,16 @@
 # Copyright (C) 2020 Arm Mbed. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 #
-from factory import Factory, Faker
+import factory
 
 from mbed_build._internal.config.config_source import ConfigSource
 
 
-class ConfigSourceFactory(Factory):
+class ConfigSourceFactory(factory.Factory):
     class Meta:
         model = ConfigSource
 
-    name = Faker("name")
-    file = Faker("file_path", extension="json")
-    config = {
-        "event-loop-size": 1024,
-        "event_loop_thread_stack_size": {"help": "Define event-loop thread stack size.", "value": 6144},
-        "http-resume-maximum-download-time-secs": {
-            "help": "Period for HTTP-resume actions after which resume gives up and terminates.",
-            "macro_name": "ARM_UC_HTTP_RESUME_MAXIMUM_DOWNLOAD_TIME_SECS",
-            "value": "(7*24*60*60)",
-        },
-    }
-    target_overrides = {
-        "*": {"target.boot-stack-size": "0x400"},
-        "STM": {"idle-thread-stack-size-debug-extra": 128},
-    }
+    name = factory.Faker("name")
+    file = factory.Faker("file_path", extension="json")
+    config = factory.Dict({})
+    target_overrides = factory.Dict({})
--- tests/_internal/config/test_config.py
+++ tests/_internal/config/test_config.py
@@ -3,41 +3,25 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 from unittest import TestCase
-import tempfile
-import pathlib
-import json
-
 
 from mbed_build._internal.config.config import Config
 from mbed_build._internal.config.config_layer import ConfigLayer
-from mbed_build._internal.config.config_source import ConfigSource
+
+from tests._internal.config.factories import ConfigSourceFactory
 
 
 class TestFromLayers(TestCase):
     def test_assembles_config_from_layers(self):
-        with tempfile.TemporaryDirectory() as directory:
-            config_data_1 = {"config": {"is-nice": {"help": "Determine if app is nice", "value": True}}}
-            source_json_1 = pathlib.Path(directory, "source_1.json")
-            source_json_1.write_text(json.dumps(config_data_1))
-            source_1 = ConfigSource.from_file(source_json_1)
-
-            config_data_2 = {"config": {"is-fast": True}, "target_overrides": {"*": {"is-nice": False}}}
-            source_json_2 = pathlib.Path(directory, "source_2.json")
-            source_json_2.write_text(json.dumps(config_data_2))
-            source_2 = ConfigSource.from_file(source_json_2)
-
-            config_data_3 = {"target_overrides": {"NOT_THIS_TARGET": {"is-nice": True}}}
-            source_json_3 = pathlib.Path(directory, "source_3.json")
-            source_json_3.write_text(json.dumps(config_data_3))
-            source_3 = ConfigSource.from_file(source_json_3)
-
+        source_1 = ConfigSourceFactory(**{"config": {"is-nice": {"help": "Determine if app is nice", "value": True}}})
+        source_2 = ConfigSourceFactory(**{"config": {"is-fast": True}, "target_overrides": {"*": {"is-nice": False}}})
+        source_3 = ConfigSourceFactory(**{"target_overrides": {"NOT_THIS_TARGET": {"is-nice": True}}})
         layer_1 = ConfigLayer.from_config_source(source_1, ["TARGET"])
         layer_2 = ConfigLayer.from_config_source(source_2, ["TARGET"])
         layer_3 = ConfigLayer.from_config_source(source_3, ["TARGET"])
 
         subject = Config.from_layers([layer_1, layer_2, layer_3])
+
         expected_config = Config(
             settings={"is-nice": {"help": "Determine if app is nice", "value": False}, "is-fast": {"value": True}},
         )
-
         self.assertEqual(subject, expected_config)
