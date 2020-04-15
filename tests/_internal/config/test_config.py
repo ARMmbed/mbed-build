4d1309fb2f6c5d14977cc23ae01c5772814c64e8
Bartek Mucha <muchzill4@gmail.com>
2020-04-15 12:05:12 +0100

    Split config assembly test

--- tests/_internal/config/test_config.py
+++ tests/_internal/config/test_config.py
@@ -13,7 +13,19 @@ from tests._internal.config.factories import ConfigSourceFactory
 class TestFromLayers(TestCase):
     def test_assembles_config_from_layers(self):
         source_1 = ConfigSourceFactory(**{"config": {"is-nice": {"help": "Determine if app is nice", "value": True}}})
-        source_2 = ConfigSourceFactory(**{"config": {"is-fast": True}, "target_overrides": {"*": {"is-nice": False}}})
+        source_2 = ConfigSourceFactory(**{"config": {"is-fast": False}})
+        layer_1 = ConfigLayer.from_config_source(source_1, ["DOES_NOT_MATTER"])
+        layer_2 = ConfigLayer.from_config_source(source_2, ["DOES_NOT_MATTER"])
+
+        subject = Config.from_layers([layer_1, layer_2])
+
+        self.assertEqual(subject.settings["is-nice"]["value"], True)
+        self.assertEqual(subject.settings["is-nice"]["help"], "Determine if app is nice")
+        self.assertEqual(subject.settings["is-fast"]["value"], False)
+
+    def test_respects_target_overrides(self):
+        source_1 = ConfigSourceFactory(**{"config": {"is-nice": True}})
+        source_2 = ConfigSourceFactory(**{"target_overrides": {"*": {"is-nice": False}}})
         source_3 = ConfigSourceFactory(**{"target_overrides": {"NOT_THIS_TARGET": {"is-nice": True}}})
         layer_1 = ConfigLayer.from_config_source(source_1, ["TARGET"])
         layer_2 = ConfigLayer.from_config_source(source_2, ["TARGET"])
@@ -21,7 +33,4 @@ class TestFromLayers(TestCase):
 
         subject = Config.from_layers([layer_1, layer_2, layer_3])
 
-        expected_config = Config(
-            settings={"is-nice": {"help": "Determine if app is nice", "value": False}, "is-fast": {"value": True}},
-        )
-        self.assertEqual(subject, expected_config)
+        self.assertEqual(subject.settings["is-nice"]["value"], False)
