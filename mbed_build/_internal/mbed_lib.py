7e762f3deefa44d3028e202ac06d6224ede51721
Bartek Mucha <muchzill4@gmail.com>
2020-04-20 15:11:15 +0100

    Stop applying board label filtering for mbed_lib.json
    Since we'll require multiple passes when assembling config,
    it'll be applied dynamically using `filter_files` at call site.

--- mbed_build/_internal/mbed_lib.py
+++ mbed_build/_internal/mbed_lib.py
@@ -6,21 +6,19 @@
 from pathlib import Path
 from typing import Iterable
 
-from mbed_build._internal.find_files import find_files, BoardLabelFilter, MbedignoreFilter
+from mbed_build._internal.find_files import find_files, MbedignoreFilter
 
 
-def find_mbed_lib_files(mbed_program_directory: Path, board_type: str) -> Iterable[Path]:
+def find_mbed_lib_files(mbed_program_directory: Path) -> Iterable[Path]:
     """Return paths to all mbed_lib.json for a given target.
 
     Args:
         mbed_program_directory: Location of mbed program
-        board_type: Name of the target to filter files for
     """
-    board_label_filter = BoardLabelFilter(board_type, mbed_program_directory)
     # Temporary workaround, which replicates hardcoded ignore rules from old tools.
     # Legacy list of ignored directories is longer, however "TESTS" and
     # "TEST_APPS" were the only ones that actually exist in the MbedOS source.
     # Ideally, this should be solved by putting an `.mbedignore` file in the root of MbedOS repo,
     # similarly to what the code below pretends is happening.
     legacy_ignore = MbedignoreFilter(("*/TESTS", "*/TEST_APPS"))
-    return find_files("mbed_lib.json", mbed_program_directory, [legacy_ignore, board_label_filter])
+    return find_files("mbed_lib.json", mbed_program_directory, [legacy_ignore])
--- tests/_internal/test_mbed_lib.py
+++ tests/_internal/test_mbed_lib.py
@@ -10,17 +10,12 @@ from mbed_build._internal.mbed_lib import find_mbed_lib_files
 
 class TestFindMbedLibFiles(TestCase):
     @mock.patch("mbed_build._internal.mbed_lib.find_files", autospec=True)
-    @mock.patch("mbed_build._internal.mbed_lib.BoardLabelFilter", autospec=True)
     @mock.patch("mbed_build._internal.mbed_lib.MbedignoreFilter", autospec=True)
-    def test_finds_mbed_lib_files_using_appropriate_filters(self, MbedignoreFilter, BoardLabelFilter, find_files):
+    def test_finds_mbed_lib_files_using_appropriate_filters(self, MbedignoreFilter, find_files):
         mbed_program_directory = Path("some-program")
-        board_type = "K64F"
 
-        subject = find_mbed_lib_files(mbed_program_directory, board_type)
+        subject = find_mbed_lib_files(mbed_program_directory)
 
         self.assertEqual(subject, find_files.return_value)
-        find_files.assert_called_once_with(
-            "mbed_lib.json", mbed_program_directory, [MbedignoreFilter.return_value, BoardLabelFilter.return_value]
-        )
+        find_files.assert_called_once_with("mbed_lib.json", mbed_program_directory, [MbedignoreFilter.return_value])
         MbedignoreFilter.assert_called_once_with(("*/TESTS", "*/TEST_APPS"))
-        BoardLabelFilter.assert_called_once_with(board_type, mbed_program_directory)
