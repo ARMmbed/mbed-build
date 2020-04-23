3d134478b4991b871d58f4f798e720145e505951
Bartek Mucha <muchzill4@gmail.com>
2020-04-23 21:31:52 +0100

    Remove single pass assembly implementation

--- mbed_build/_internal/config/assemble_config.py
+++ mbed_build/_internal/config/assemble_config.py
@@ -2,30 +2,16 @@
 # Copyright (C) 2020 Arm Mbed. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 #
+"""Configuration assembly."""
 import pathlib
-from typing import Callable, List
 
-from mbed_build._internal.config.config import Config, TargetMetadata
-from mbed_build._internal.config.source import Source
-from mbed_build._internal.find_files import find_files, filter_files, LabelFilter
+from mbed_build._internal.config.config import Config
 
 
 def assemble_config(mbed_target: str, mbed_program_directory: pathlib.Path) -> Config:
-    # Initial config is required to calculate first pass filters
-    mbed_target_source = Source.from_target(mbed_target, mbed_program_directory)
-    config = Config.from_sources([mbed_target_source])
+    """Assemble Config for given target and program directory.
 
-    filters = _build_filters(config.target_metadata)
-    all_mbed_lib_files = find_files("mbed_lib.json", mbed_program_directory)
-    filtered_lib_files = filter_files(all_mbed_lib_files, filters)
-    mbed_lib_sources = [Source.from_mbed_lib(file, config.target_metadata.labels) for file in filtered_lib_files]
-    config = Config.from_sources([mbed_target_source] + mbed_lib_sources)
-    return config
-
-
-def _build_filters(target_metadata: TargetMetadata) -> List[Callable]:
-    return [
-        LabelFilter("TARGET", target_metadata.labels),
-        LabelFilter("FEATURE", target_metadata.features),
-        LabelFilter("COMPONENT", target_metadata.components),
-    ]
+    The structure and configuration of MbedOS requires us to do multiple passes over
+    configuration assembly, as each Source can modify filtering rules applied to finding libs.
+    """
+    return Config()
