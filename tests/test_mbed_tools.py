91ee4ac3b8c1ea6dbcc03ac6f862f7394320a4c5
Rob Walton <rob.walton@arm.com>
2020-07-14 13:36:13 +0100

    Remove config command (#51)
    * Remove config command from mbed_tools.py
    
    * Remove config command from __main__.py
    
    * Remove mbed_tools/config.py
    
    * Remove config command tests

--- tests/test_mbed_tools.py
+++ tests/test_mbed_tools.py
@@ -5,13 +5,7 @@
 from unittest import TestCase
 
 from mbed_build._internal.mbed_tools.export import export
-from mbed_build._internal.mbed_tools.config import config
 from mbed_build import mbed_tools
-
-
-class TestConfig(TestCase):
-    def test_aliases_config(self):
-        self.assertEqual(mbed_tools.config, config)
 
 
 class TestExport(TestCase):
